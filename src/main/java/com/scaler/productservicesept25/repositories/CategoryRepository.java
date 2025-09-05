30a1417d5777c8847c511fba28174079d838dde4
Preeti <preeti.kumari241116@gmail.com>
2025-09-05 09:52:33 +0530

    Part 2-> ImplementSelfProductService

---
+++ src/main/java/com/scaler/productservicesept25/repositories/CategoryRepository.java
@@ -0,0 +1,24 @@
+package com.scaler.productservicesept25.repositories;
+
+import com.scaler.productservicesept25.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long>{
+
+    @Override
+    Optional<Category> findById(Long aLong); //select * from categories where id=?
+
+    List<Category> findByTitle(String title); //select * from categories where title=?
+
+    List<Category> findByTitleContainsIgnoreCase(String title); //select * from categories where title like '%title%';
+
+    @Query("select c.title from categories c where c.id=?1")
+    Optional<Category> findOnlyCategoryTitleById(Long categoryId);
+
+}
--- src/main/java/com/scaler/productservicesept25/repositories/ProductRepository.java
+++ src/main/java/com/scaler/productservicesept25/repositories/ProductRepository.java
@@ -1,7 +1,33 @@
 package com.scaler.productservicesept25.repositories;
 
+import com.scaler.productservicesept25.models.Category;
+import com.scaler.productservicesept25.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public class ProductRepository {
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    @Override
+    Optional<Product> findById(Long aLong); //select * from products where id=?
+
+    List<Product> findByTitle(String title); //select * from products where title=?
+
+    List<Product> findByTitleContainsIgnoreCase(String title); //select * from products where title like '%title%';
+
+    List<Product> findByPriceBetween(Double priceAfter, Double priceBefore); //select * from products where price between ? and ?;
+
+    List<Product> findByCategory(Category category); //select * from products where category_id=?;
+
+    List<Product> findByCategory_Id(Long categoryId); //select * from products where category_id=?;
+    
+    List<Product> findByCategoryTitle(String categoryTitle); //select * from products p join categories c on p.category_id=c.id where c.title=?;
+
+    @Query("select p.title from products p where p.id=?1")
+    Optional<Product> findOnlyProductTitleById(Long productId); //select title from products where id=?;
+
 }
