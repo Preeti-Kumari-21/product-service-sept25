4b88a76631b62afa25c38ae2f27bd1e3c149dac6
Preeti <preeti.kumari241116@gmail.com>
2025-09-08 11:45:32 +0530

    Part 2-> DbMigration, Queries and Query Methods

--- src/main/java/com/scaler/productservicesept25/repositories/ProductRepository.java
+++ src/main/java/com/scaler/productservicesept25/repositories/ProductRepository.java
@@ -4,6 +4,7 @@ import com.scaler.productservicesept25.models.Category;
 import com.scaler.productservicesept25.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -34,4 +35,7 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
 
     @Override
     void deleteById(Long productId);
+
+    @Query(value = "select * from products p where p.id=:id", nativeQuery = true)
+    Product findProductWithGivenId(@Param("id") long productId);
 }
--- src/test/java/com/scaler/productservicesept25/ProductServiceSept25ApplicationTests.java
+++ src/test/java/com/scaler/productservicesept25/ProductServiceSept25ApplicationTests.java
@@ -1,13 +1,36 @@
 package com.scaler.productservicesept25;
 
+import com.scaler.productservicesept25.models.Product;
+import com.scaler.productservicesept25.repositories.ProductRepository;
+import com.scaler.productservicesept25.services.ProductService;
+import org.junit.jupiter.api.AutoClose;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class ProductServiceSept25ApplicationTests {
 
+    @Autowired
+    ProductRepository productRepository;
+
+/*    public ProductServiceSept25ApplicationTests(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }*/
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testProductService(){
+        Optional<Product> product1 = productRepository.findById(1L);
+        System.out.println("DEBUG-1");
+
+        Product product = productRepository.findProductWithGivenId(1L);
+        System.out.println("DEBUG-2");
     }
 
 }
