36f2181ee7cedfdd3497a5282b85c91462995d15
Preeti <preeti.kumari241116@gmail.com>
2025-09-09 07:14:23 +0530

    Part 6 -> FetchTypes and Models

--- src/main/java/com/scaler/productservicesept25/models/Category.java
+++ src/main/java/com/scaler/productservicesept25/models/Category.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class Category extends BaseModel{
     private String title;
 
-    /*@OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
-    private List<Product> products;*/
+    //@OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
--- src/test/java/com/scaler/productservicesept25/ProductServiceSept25ApplicationTests.java
+++ src/test/java/com/scaler/productservicesept25/ProductServiceSept25ApplicationTests.java
@@ -1,13 +1,18 @@
 package com.scaler.productservicesept25;
 
+import com.scaler.productservicesept25.models.Category;
 import com.scaler.productservicesept25.models.Product;
+import com.scaler.productservicesept25.repositories.CategoryRepository;
 import com.scaler.productservicesept25.repositories.ProductRepository;
 import com.scaler.productservicesept25.services.ProductService;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.AutoClose;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.beans.Transient;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -15,6 +20,9 @@ class ProductServiceSept25ApplicationTests {
 
     @Autowired
     ProductRepository productRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
 
 /*    public ProductServiceSept25ApplicationTests(ProductRepository productRepository) {
         this.productRepository = productRepository;
@@ -32,5 +40,28 @@ class ProductServiceSept25ApplicationTests {
         Product product = productRepository.findProductWithGivenId(1L);
         System.out.println("DEBUG-2");
     }
+
+    @Test
+    public void checkProductFetchTypes(){
+        System.out.println("DEBUG-3");
+        Optional<Product> optionalProduct = productRepository.findById(1L); // No collection attributes in Product model
+        //Hence no lazy loading
+        //EAGER fetch
+    }
+
+    @Test
+    @Transactional
+    public void checkCategoryFetchTypes(){
+        System.out.println("DEBUG-4");
+        Optional<Category> optionalCategory = categoryRepository.findById(1L); //LAZY fetch
+
+        //Using the below line we are trying to access the collection attribute of Category if and only if user wants the products detail
+        List<Product> products = optionalCategory.get().getProducts();
+
+        System.out.println("DEBUG-5");
+        System.out.println(products.get(0).getCategory());
+        //select * from products where category_id = 1
+    }
+
 
 }
